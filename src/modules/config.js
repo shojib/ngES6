123630d0f0c51161da055c0b4003fbf40796c66f
mhossain <mhossain@amplify.com>
2015-04-22 12:20:27 -0400

    adding ngAnimate

--- src/modules/config.js
+++ src/modules/config.js
@@ -23,6 +23,7 @@ System.config({
     "angular-translate/bower-angular-translate-storage-cookie": "github:angular-translate/bower-angular-translate-storage-cookie@2.6.1",
     "angular-translate/bower-angular-translate-storage-local": "github:angular-translate/bower-angular-translate-storage-local@2.6.1",
     "angular-ui/ui-router": "github:angular-ui/ui-router@0.2.13",
+    "angular/bower-angular-animate": "github:angular/bower-angular-animate@1.3.15",
     "babel": "npm:babel-core@5.1.11",
     "babel-runtime": "npm:babel-runtime@5.1.11",
     "components/angular-resource": "github:components/angular-resource@1.2.0",
@@ -41,6 +42,9 @@ System.config({
       "angular": "github:angular/bower-angular@1.3.15"
     },
     "github:angular-ui/ui-router@0.2.13": {
+      "angular": "github:angular/bower-angular@1.3.15"
+    },
+    "github:angular/bower-angular-animate@1.3.15": {
       "angular": "github:angular/bower-angular@1.3.15"
     },
     "github:angular/bower-angular-cookies@1.3.15": {
